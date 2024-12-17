2a76b8a6a9d0530ccf448aa52d15928dfc3981b9
陈昱翔 <148471006+xinghenLuyus@users.noreply.github.com>
2024-12-17 17:55:58 +0800

    2

--- src/main.js
+++ src/main.js
@@ -1,18 +1,7 @@
-// import Vue from 'vue'
-// import App from './App.vue'
-// import router from './router/router'
-
-// Vue.config.productionTip = false
-
-// new Vue({
-//   router,
-//   render: h => h(App)
-// }).$mount('#app')
-
-import {createApp} from 'vue'
-import router from './router'
+import { createApp } from 'vue'
+import router from './router/router'
 import App from './App.vue'
- 
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
