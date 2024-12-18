303d1fe08fa566bb2a2c1dd652eb2c0283d785d8
wyc <2954258298@qq.com>
2024-12-18 20:42:58 +0800

    1233456788

--- src/components/RecomPage.vue
+++ src/components/RecomPage.vue
@@ -2,11 +2,13 @@
     <div>
         <!-- 头部start -->
         <div id="header">
-            <img src="@/assets/picture/图标/返回.png" alt="" id="return">
-            <div id="header-button">
-            <button>推荐作品</button>
-            <button>推荐画师</button>
-            <button>推荐漫画</button>
+            <div id="header-lfet">
+                <router-link to="/" id="return"><img src="@/assets/picture/图标/返回.png" alt="" id="return"></router-link>
+                <div id="header-button">
+                    <button @click="scrollTo('work1')">推荐作品</button>
+                    <button @click="scrollTo('work2')">推荐画师</button>
+                    <button @click="scrollTo('work3')">推荐漫画</button>
+                </div>
             </div>
             <div id="search">
             <input type="text" placeholder="搜索">
@@ -16,6 +18,7 @@
     
         <!-- 作品集start -->
         <div id="works">
+            <a name="work1"></a>
             <div id="label">作品集</div>
             <div id="works-inside">
             <!-- 作品一start -->
@@ -36,7 +39,7 @@
                     <img src="@/assets/picture/头像/1.jpg" alt="">
                     <p id="nichen">花子枯</p>
                 </div>
-                <input type="button" id="guanzhu" value="关注">
+                <input type="button" id="guanzhu" :value="guanzhuStatus['1']" @click="toggleGuanzhu('1')">
                 </div>
             </div>
             <!-- 作品一end -->
@@ -59,7 +62,7 @@
                     <img src="@/assets/picture/头像/2.jpg" alt="">
                     <p id="nichen">二分法</p>
                 </div>
-                <input type="button" id="guanzhu" value="关注">
+                <input type="button" id="guanzhu" :value="guanzhuStatus['2']" @click="toggleGuanzhu('2')">
                 </div>
             </div>
             <!-- 作品二end -->
@@ -82,7 +85,7 @@
                     <img src="@/assets/picture/头像/3.jpg" alt="">
                     <p id="nichen">二分法</p>
                 </div>
-                <input type="button" id="guanzhu" value="关注">
+                <input type="button" id="guanzhu" :value="guanzhuStatus['3']" @click="toggleGuanzhu('3')">
                 </div>
             </div>
             <!-- 作品三end -->
@@ -94,6 +97,7 @@
         <!-- 作品集end -->
     
         <!-- 画师集start -->
+        <a name="work2"></a>
         <div id="painter">
             <div id="label">画师集</div>
             <div id="painter-inside">
@@ -105,7 +109,7 @@
                     <img src="@/assets/picture/头像/4.jpg" alt="">
                     <p id="nichen">希田</p>
                 </div>
-                <input type="button" id="guanzhu" value="关注">
+                <input type="button" id="guanzhu" :value="guanzhuStatus['4']" @click="toggleGuanzhu('4')">
                 </div>
             </div>
             <!-- 画师一end -->
@@ -118,7 +122,7 @@
                     <img src="@/assets/picture/头像/5.jpg" alt="">
                     <p id="nichen">echo</p>
                 </div>
-                <input type="button" id="guanzhu" value="关注">
+                <input type="button" id="guanzhu" :value="guanzhuStatus['5']" @click="toggleGuanzhu('5')">
                 </div>
             </div>
             <!-- 画师二end -->
@@ -131,7 +135,7 @@
                     <img src="@/assets/picture/头像/6.jpg" alt="">
                     <p id="nichen">茯苓那</p>
                 </div>
-                <input type="button" id="guanzhu" value="关注">
+                <input type="button" id="guanzhu" :value="guanzhuStatus['6']" @click="toggleGuanzhu('6')">
                 </div>
             </div>
             <!-- 画师三end -->
@@ -143,6 +147,7 @@
         <!-- 画师集end -->
     
         <!-- 漫画集start -->
+        <a name="work3"></a>
         <div id="cartoon">
             <div id="label">漫画集</div>
             <div id="cartoon-inside">
@@ -181,6 +186,7 @@
             友情链接 <a href="">动漫评论</a> <a href="">异次元</a> <a href="">漫展</a> <a href="">高校动漫网</a> <a href="">二次元现场</a><br>
             不良信息举报电话:12312312312 | 举报邮箱:ecy@zzz.com| 客服微信:yuanyuann丨 行政办公地址:将次元打开之地<br>
             </p>
+            <div></div>
         </div>
         <!-- 页脚end -->
     </div>
@@ -188,7 +194,30 @@
   
 <script>
 export default {
-name: 'RecomPage'
+    name: 'RecomPage',
+    data() {
+        return {
+            guanzhuStatus: {
+                '1': '关注',
+                '2': '关注',
+                '3': '关注',
+                '4': '关注',
+                '5': '关注',
+                '6': '关注'
+            }
+        }
+    },
+    methods: {
+        toggleGuanzhu(id) {
+            this.guanzhuStatus[id] = this.guanzhuStatus[id] === '关注' ? '已关注' : '关注';
+        },
+        scrollTo(anchor) {
+            const element = document.querySelector(`a[name="${anchor}"]`);
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    }
 }
 </script>
   
