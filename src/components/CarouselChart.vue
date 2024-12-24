db0dcb041eb0ca51b29369ed5efee0fa4b970e66
wyc <2954258298@qq.com>
2024-12-24 14:57:52 +0800

    图片轮播可选but不自适应

--- src/components/CarouselChart.vue
+++ src/components/CarouselChart.vue
@@ -1,142 +1,191 @@
-<!-- 图片轮播图可选 -->
-
 <template>
-    <div>
-        <div  class="banner">
-            <img src="@/assets/picture/登录界面/01.jpg" class="img">
-            <div v-for="(title,titleIndex) in titles" :key="titleIndex"  class="content">
-                <transition name="scale">
-                    <div v-show="imgIndex===titleIndex" >
-                      <span>{{title}}</span>
-                    </div>
-                  </transition>
-            </div>
-            <div class="dot-content">
-                <div v-for="(item,index) in imgArr.length" :key="index" :class="index===imgIndex?'active':'dot-box'" @click="selectImg(index)">
-                </div>
-            </div>
-            <el-icon size="40" class="left-btn" @click="next(-1)"><ArrowLeftBold /></el-icon>
-            <el-icon size="40" class="right-btn" @click="next(1)"><ArrowRightBold /></el-icon>
+    <div id="CarouselChart">
+      <div class="banner">
+        <img :src="currentImage" class="img">
+
+        <div class="dot-content">
+          <div v-for="(item, index) in imgArr.length" :key="index" :class="index === imgIndex ? 'active' : 'dot-box'" @click="selectImg(index)">
+          </div>
         </div>
+
+        <div class="left-btn" @click="next(-1)">
+          <img src="@/assets/picture/left.png" alt="" id="left-icon">
+        </div>
+
+        <div class="right-btn" @click="next(1)">
+          <img src="@/assets/picture/right.png" alt="" id="right-icon">
+        </div>
+        
+      </div>
     </div>
-</template>
-
-<script setup>
-import { ref } from 'vue';
-
-const imgIndex = ref(0);
-const imgArr = [
-    '@/assets/picture/登录界面/01.jpg',
-    '@/assets/picture/登录界面/02.jpg',
-    '@/assets/picture/登录界面/03.jpg'
-];
-const titles = [
-    '🍑🍑🍑🍑🍑🍑','⚪⚪⚪⚪⚪⚪','❀❀❀❀❀❀'
-];
-const selectImg = (e) => {
-    let currentImg = document.querySelector('.img');
-    currentImg.style.opacity = '0';
-    setTimeout(() => {
-        currentImg.src = imgArr[e];
-        imgIndex.value = e;
-        currentImg.style.opacity = '1';
-    }, 500);
-};
-const next = (e) => {
-    e > 0 ? imgIndex.value += 1 : imgIndex.value -= 1;
-    if (imgIndex.value >= imgArr.length) {
-        imgIndex.value = 0;
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  
+  export default {
+    name: 'CarouselChart',
+    setup() {
+      const imgIndex = ref(0);
+      const imgArr = [
+        require('@/assets/picture/登录界面/01.jpg'),
+        require('@/assets/picture/登录界面/02.jpg'),
+        require('@/assets/picture/登录界面/03.jpg')
+      ];
+  
+      const currentImage = ref(imgArr[imgIndex.value]);
+  
+      const selectImg = (e) => {
+        let currentImg = document.querySelector('.img');
+        currentImg.style.opacity = '0';
+        setTimeout(() => {
+          currentImage.value = imgArr[e];
+          imgIndex.value = e;
+          currentImg.style.opacity = '1';
+        }, 500);
+      };
+  
+      const next = (e) => {
+        e > 0 ? imgIndex.value += 1 : imgIndex.value -= 1;
+        if (imgIndex.value >= imgArr.length) {
+          imgIndex.value = 0;
+        }
+        if (imgIndex.value < 0) {
+          imgIndex.value = imgArr.length - 1;
+        }
+        selectImg(imgIndex.value);
+      };
+  
+      return {
+        imgIndex,
+        imgArr,
+        currentImage,
+        selectImg,
+        next
+      };
     }
-    if (imgIndex.value < 0) {
-        imgIndex.value = imgArr.length - 1;
-    }
-    selectImg(imgIndex.value);
-};
-</script>
-
-
-<style scoped lang="less">
-.banner  {
+  }
+  </script>
+  
+  <style scoped>
+  #left-icon, #right-icon {
+    width: 20%;
+  }
+  
+  .banner {
     position: relative;
     display: flex;
     justify-content: center;
     width: 100%;
-    .left-btn, .right-btn {
-        width:  0;
-    }
-    &:hover {
-        .left-btn {
-            position: absolute;
-            cursor: pointer;
-            top: 50%;
-            left: 30px;
-            width: auto;           
-    
-        }
-        .right-btn {
-            position: absolute;
-            cursor: pointer;
-            top: 50%;
-            right: 30px;            
-            width: auto;
-        }
-    }
-    .img {
-        display: none;
-        &:first-child {
-            display: block;
-            height: 60vh;
-            opacity: 1;
-            transition: opacity 0.5s ease;
-        }
-    
-    }
-    .scale-enter-active{
-        transition: all 1s ease-in-out;
-      }
-      
-      .scale-leave-active {
-        transition: all 1s linear;
-      }
-      
-      .scale-enter-from{
-        margin-right: 300px;
-        opacity: 0;
-      }
-      .scale-leave-to {
-        opacity: 0;
-      }
-    .content {
-        position: absolute;
-        top: 50%;
-        font-size: 40px;
-        color: #fff;
-    }
- 
-    .dot-content {
-        display: flex;
-        position: absolute;
-        bottom: 20px;
-        justify-content: space-around;
-        align-items: center;
-        width: 100px;
-        height: 30px;
- 
-        .dot-box {
-            width: 20px;
-            height: 20px;
-            border-radius: 50%;
-            background-color: #fff;
-            cursor: pointer;
-        }
-        .active {
-            cursor: pointer;
-            width: 20px;
-            height: 20px;
-            border-radius: 50%;
-            background-color: red;
-        }
-    }
- 
+  }
+  
+  .left-btn, .right-btn {
+    width: 0;
+  }
+  
+  .banner:hover .left-btn {
+    position: absolute;
+    cursor: pointer;
+    top: 50%;
+    left: 30px;
+    width: auto;
+  }
+  
+  .banner:hover .right-btn {
+    position: absolute;
+    cursor: pointer;
+    top: 50%;
+    right: -30px;
+    width: auto;
+  }
+  
+  .img {
+    display: block;
+    height: 60vh;
+    opacity: 1;
+    transition: opacity 0.5s ease;
+  }
+  
+  .dot-content {
+    display: flex;
+    position: absolute;
+    bottom: 20px;
+    justify-content: space-around;
+    align-items: center;
+    width: 100px;
+    height: 30px;
+  }
+  
+  .dot-box {
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: #fff;
+    cursor: pointer;
+  }
+  
+  .active {
+    cursor: pointer;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: red;
+  }
+
+  
+@media (max-width: 1200px) {
+  .img {
+    height: 50vh;
+  }
+
+  #left-icon, #right-icon {
+    width: 15%;
+  }
+
+  .dot-content {
+    width: 80px;
+    height: 25px;
+  }
+
+  .dot-box, .active {
+    width: 15px;
+    height: 15px;
+  }
 }
-</style>
+
+@media (max-width: 768px) {
+  .img {
+    height: 40vh;
+  }
+
+  #left-icon, #right-icon {
+    width: 10%;
+  }
+
+  .dot-content {
+    width: 60px;
+    height: 20px;
+  }
+
+  .dot-box, .active {
+    width: 10px;
+    height: 10px;
+  }
+}
+
+@media (max-width: 480px) {
+  .img {
+    height: 30vh;
+  }
+
+  .dot-content {
+    width: 50px;
+    height: 15px;
+  }
+
+  .dot-box, .active {
+    width: 8px;
+    height: 8px;
+  }
+}
+  </style>
